8b9bdd4c5a258fb8b22acbb7a7a257d77b36b1b4
kgupta1542 <35873683+kgupta1542@users.noreply.github.com>
2018-06-08 17:54:05 -0700

    Add files via upload

--- scout.php
+++ scout.php
@@ -10,17 +10,22 @@
 		<h1 align="center"><span style="color:mediumslateblue">Ready to Scout?</span></h1>
 		<hr>
 
-		<div id="side_bar" class="fixed">
-			<p id="team_nm" class="fixed">Team Number: </p>
-				<br/>
-				<br/>
-			<p id="match_nm" class="fixed">Match Number: </p>
-				<br/>
-				<br/>
-			<p id="alliance" class="fixed">Alliance: </p>
+		<div id="side_bar" class="absolute">
+			<div id="side_bar_contents" class="fixed">
+				<p class="fixed">Match Number:</p>
+					<p id="match_nm" class="fixed"></p>
+						<br/>
+						<br/>
+				<p class="fixed">Team Number:</p>
+					<p id="team_nm" class="fixed"></p>
+						<br/>
+						<br/>
+				<p class="fixed">Alliance:</p>
+					<p id="alliance" class="fixed"></p>
+			</div>
 		</div>
 		
-		<div id="txt_Hint" class="inline">	
+		<div id="pre_game" class="scout">	
        		<p align="center">
       			<input class="report" type="number" id="match_num" name="match_nm" placeholder="Match Number">
        				<br/>
@@ -29,26 +34,61 @@
        				<br/>
        				<br/>
        			Alliance Color:
-       			<div class="btn_group">
+       			<div id="alliance_sel" class="btn_group">
        				<p align="center">
-       					<button name="red" class="btn red">Red</button>
-       					<button name="blue" class="btn blue">Blue</button>
+       					<button name="Red" class="btn light red">Red</button>
+       					<button name="Blue" class="btn light blue">Blue</button>
        				</p>
        			</div>
 				
 				<p align="center">Starting Position:</p>
-				<div class="btn_group">
+				<div id="start_pos" class="btn_group">
 					<p align="center">
-						<button class="btn">Left</button>
-						<button class="btn">Center</button>
-						<button class="btn">Right</button>
+						<button name="Left" class="light btn">Left</button>
+						<button name="Center" class="light btn">Center</button>
+						<button name="Right" class="light btn">Right</button>
 					</p>
 				</div>
 				<br/>
+				
         		<p align="center">
-        			<button class="btn">Tele-Op ></button>
+        			<button id="auto_btn" class="btn">Auto ></button>
         		</p>
         	</p>
+        </div>
+        
+        <div id="auto" class="scout display">	
+       		<p align="center">Baseline:</p>
+       		<div id="baseline_sel" class="btn_group">
+       			<p align="center">
+       				<button name="Yes" class="btn light">Yes</button>
+       				<button name="No" class="btn light">No</button>
+       			</p>
+       		</div>
+				
+			<p align="center">Switch:</p>
+       		<div id="baseline_sel" class="btn_group_bi">
+       			<p align="center">
+       				<button name="+" class="btn plus">+</button>
+       				<button name="-" class="btn minus">-</button>
+       			</p>
+       		</div>			
+        	
+        	<p align="center">Scale:</p>
+       		<div id="baseline_sel" class="btn_group_bi">
+       			<p align="center">
+       				<button name="+" class="btn plus">+</button>
+       				<button name="-" class="btn minus">-</button>
+       			</p>
+       		</div>			
+       		<br/>
+        	
+        	<div class="btn_group">
+        		<p align="center">
+        			<button id="auto_btn" class="btn">< Pre-Game</button>
+        			<button id="auto_btn" class="btn">Tele-Op ></button>
+        		</p>
+        	</div>
         </div>
 	</body>
 </html>
--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+	var match_number;
+	var team_number;
+	var alliance = "";
+	var start_pos = "";
+	
     $(".switch").click(function(){
     	$("#signup").toggleClass("display");
     	$("#login").toggleClass("display");
@@ -6,14 +11,33 @@ $(document).ready(function(){
     $("#dropdown").click(function(){
 		$("#personal").toggleClass("display").toggleClass("inline-block");
     });
-    $(".btn").click(function(){
-        $(this).css("opacity", 1);
-    });
     $("#match_num").change(function(){
-    	$("#match_nm").text("Match Number: " + $("#match_num").val());
+    	match_number = $("#match_num").val();
+    	$("#match_nm").text(match_number);
     });
     $("#team_num").change(function(){
-    	$("#team_nm").text("Team Number: " + $("#team_num").val());
+    	team_number = $("#team_num").val();
+    	$("#team_nm").text(team_number);
+    });
+    $(".btn_group .btn").click(function(){
+        $(this).siblings().addClass("light"); 
+        $(this).removeClass("light");
+    });
+    $("#alliance_sel .btn").click(function(){
+    	alliance = $(this).attr("name");
+    	$("#alliance").text(alliance);
+    });
+    $("#start_pos .btn").click(function(){
+    	start_pos = $(this).attr("name");
+    });
+    $("#auto_btn").click(function(){
+    	if(match_number > 0 && team_number > 0 && alliance !== "" && start_pos !== ""){
+    		$("#pre_game").toggleClass("display");
+    		$("#auto").toggleClass("display");
+    	}
+    	else{
+    		alert("Please fill in all information before moving on!");
+    	}
     });
 });
 
