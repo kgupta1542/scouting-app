61f6f00971844001cc5217d064f0e6210a51e27c
kgupta1542 <35873683+kgupta1542@users.noreply.github.com>
2018-06-08 20:30:50 -0700

    Add files via upload

--- scout.php
+++ scout.php
@@ -25,7 +25,7 @@
 			</div>
 		</div>
 		
-		<div id="pre_game" class="scout display">	
+		<div id="pre_game" class="scout">	
        		<p align="center">
       			<input class="report" type="number" id="match_num" name="match_nm" placeholder="Match Number">
        				<br/>
@@ -33,7 +33,7 @@
        			<input class="report" type="number" id="team_num" name="team_nm" placeholder="Team Number">
        				<br/>
        				<br/>
-       			Alliance Color:
+       			<p align="center">Alliance Color:</p>
        			<div id="alliance_sel" class="btn_group">
        				<p align="center">
        					<button name="Red" class="btn light red">Red</button>
@@ -57,7 +57,7 @@
         	</p>
         </div>
         
-        <div id="auto" class="scout">	
+        <div id="auto" class="scout display">	
        		<p align="center">Baseline:</p>
        		<div id="baseline_sel" class="btn_group">
        			<p align="center">
@@ -89,10 +89,94 @@
         	
         	<div class="btn_group">
         		<p align="center">
-        			<button id="pregame_btn" class="btn">< Pre-Game</button>
+        			<button id="pregame_back_btn" class="btn">< Pre-Game</button>
         			<button id="teleop_btn" class="btn">Tele-Op ></button>
         		</p>
         	</div>
+        </div>
+        
+        <div id="teleop" class="scout display">	
+       		<p align="center">Personal Switch:
+				<span id="switch_personal">0</span>
+			</p>
+       		<div id="switch_personal_sel" class="btn_group_bi">
+       			<p align="center">
+       				<button name="plus" class="btn plus">+</button>
+       				<button name="minus" class="btn minus">-</button>
+       			</p>
+       		</div>	
+				
+			<p align="center">Opponent Switch:
+				<span id="switch_other">0</span>
+			</p>
+       		<div id="switch_other_sel" class="btn_group_bi">
+       			<p align="center">
+       				<button name="plus" class="btn plus">+</button>
+       				<button name="minus" class="btn minus">-</button>
+       			</p>
+       		</div>			
+        	
+        	<p align="center">Scale:
+        		<span id="scale">0</span>
+        	</p>
+       		<div id="scale_sel" class="btn_group_bi">
+       			<p align="center">
+       				<button name="plus" class="btn plus">+</button>
+       				<button name="minus" class="btn minus">-</button>
+       			</p>
+       		</div>
+       		
+       		<p align="center">Exchange:
+        		<span id="exchange">0</span>
+        	</p>
+       		<div id="exchange_sel" class="btn_group_bi">
+       			<p align="center">
+       				<button name="plus" class="btn plus">+</button>
+       				<button name="minus" class="btn minus">-</button>
+       			</p>
+       		</div>			
+       		<br/>
+        	
+        	<div class="btn_group">
+        		<p align="center">
+        			<button id="auto_back_btn" class="btn">< Auto</button>
+        			<button id="endgame_btn" class="btn">End Game ></button>
+        		</p>
+        	</div>
+        </div>
+        
+        <div id="endgame" class="scout display">
+        	<p align="center">End Status:</p>
+			<div id="endgame_sel" class="btn_group">
+				<p align="center">
+					<button name="Climb" class="light btn">Climb</button>
+					<button name="Park" class="light btn">Park</button>
+					<button name="None" class="light btn">None</button>
+				</p>
+			</div>
+			
+			<p align="center">Result</p>
+       		<div id="result_sel" class="btn_group">
+       			<p align="center">
+       				<button name="Won" class="btn light">Won</button>
+       				<button name="Lost" class="btn light">Lost</button>
+       			</p>
+       		</div>
+       		
+       		<p align="center">Comments?</p>
+       		<div id="comments_sel">
+       			<p align="center">
+       				<textarea id="comments" name="comments" rows="6" cols="65"></textarea>			
+  				</p>
+  			</div>  
+  			<br/>
+  			
+  			<div class="btn_group">
+        		<p align="center">
+        			<button id="teleop_back_btn" class="btn">< Tele-op</button>
+        			<button id="submit_btn" class="btn">Submit ></button>
+        		</p>
+        	</div>    
         </div>
 	</body>
 </html>
--- script.js
+++ script.js
@@ -1,43 +1,72 @@
-$(document).ready(function(){
-	//Define all variables so that they are global
-	var match_number;
-	var team_number;
-	var alliance = "";
-	var start_pos = "";
-	var baseline = "";
-	var switch_auto = 0;
-	var scale_auto = 0;
+//Define all variables so that they are global
+var match_number;
+var team_number;
+var alliance = "";
+var start_pos = "";
+var baseline = "";
+var switch_auto = 0;
+var scale_auto = 0;
+var switch_personal = 0;
+var switch_other = 0;
+var scale = 0;
+var exchange = 0;
+var endgame = "";
+var result = "";
+var comments = "";
 
+//AJAX submisstion function
+function submitData() {
+	var xhttp;    
+	xhttp = new XMLHttpRequest();
+	xhttp.open("GET", "http://localhost/C_Biscuit/submit.php?mn="+match_number
+			+"&tn="+team_number
+			+"&a="+alliance
+			+"&start="+start_pos
+			+"&bl="+baseline
+			+"&swa="+switch_auto
+			+"&sca="+scale_auto
+			+"&swp="+switch_personal
+			+"&swo="+switch_other
+			+"&sc="+scale
+			+"&ex="+exchange
+			+"&end="+endgame
+			+"&res="+result
+			+"comments="+comments, true);
+	xhttp.send();
+}
+
+//JQuery scripts
+$(document).ready(function(){
 	//General scripts
-    $(".switch").click(function(){
+    $(".switch").click(function(){//Toggle between log-in and sign-up
     	$("#signup").toggleClass("display");
     	$("#login").toggleClass("display");
 	});
-    $("#dropdown").click(function(){
+    $("#dropdown").click(function(){//Dropdown personal menu
 		$("#personal").toggleClass("display").toggleClass("inline-block");
     });
-    
-    //Pre-game scouting scripts
-    $("#match_num").change(function(){
-    	match_number = $("#match_num").val();
-    	$("#match_nm").text(match_number);
-    });
-    $("#team_num").change(function(){
-    	team_number = $("#team_num").val();
-    	$("#team_nm").text(team_number);
-    });
-    $(".btn_group .btn").click(function(){
+    $(".btn_group .btn").click(function(){//Button effects
         $(this).siblings().addClass("light"); 
         $(this).removeClass("light");
     });
-    $("#alliance_sel .btn").click(function(){
+    
+    //Pre-game scouting scripts
+    $("#match_num").change(function(){//Save match number
+    	match_number = $("#match_num").val();
+    	$("#match_nm").text(match_number);
+    });
+    $("#team_num").change(function(){//Save team number
+    	team_number = $("#team_num").val();
+    	$("#team_nm").text(team_number);
+    });
+    $("#alliance_sel .btn").click(function(){//Save alliance color
     	alliance = $(this).attr("name");
     	$("#alliance").text(alliance);
     });
-    $("#start_pos .btn").click(function(){
+    $("#start_pos .btn").click(function(){//Save starting position
     	start_pos = $(this).attr("name");
     });
-    $("#auto_btn").click(function(){
+    $("#auto_btn").click(function(){//Go on to Auto scouting
     	if(match_number > 0 && team_number > 0 && alliance !== "" && start_pos !== ""){
     		$("#pre_game").toggleClass("display");
     		$("#auto").toggleClass("display");
@@ -48,10 +77,10 @@ $(document).ready(function(){
     });
     
     //Auto scouting scripts
-    $("#baseline_sel .btn").click(function(){
+    $("#baseline_sel .btn").click(function(){//Save baseline boolean
     	baseline = $(this).attr("name");
     })
-    $("#switch_auto_sel .btn").click(function(){
+    $("#switch_auto_sel .btn").click(function(){//Save switch amount in auto
     	if($(this).attr("name") == "plus"){
     		switch_auto = switch_auto + 1;
     	}
@@ -61,7 +90,7 @@ $(document).ready(function(){
     	
     	$("#switch_auto").text(switch_auto);
     });
-    $("#scale_auto_sel .btn").click(function(){
+    $("#scale_auto_sel .btn").click(function(){//Save scale amount in auto
     	if($(this).attr("name") == "plus"){
     		scale_auto = scale_auto + 1;
     	}
@@ -71,7 +100,11 @@ $(document).ready(function(){
     	
     	$("#scale_auto").text(scale_auto);
     });
-    $("#teleop_btn").click(function(){
+    $("#pregame_back_btn").click(function(){//Return to Pre-Game scouting
+    	$("#pre_game").toggleClass("display");
+    	$("#auto").toggleClass("display");
+    });
+    $("#teleop_btn").click(function(){//Go on to Tele-Op scouting
     	if(baseline !== "" && switch_auto >= 0 && scale_auto >= 0){
     		$("#auto").toggleClass("display");
     		$("#teleop").toggleClass("display");
@@ -82,5 +115,80 @@ $(document).ready(function(){
     });
     
     //Teleop scouting scripts
+    $("#switch_personal_sel .btn").click(function(){//Save personal switch amount
+    	if($(this).attr("name") == "plus"){
+    		switch_personal = switch_personal + 1;
+    	}
+    	else{
+    		switch_personal = switch_personal - 1;
+    	}
+    	
+    	$("#switch_personal").text(switch_personal);
+    });
+    $("#switch_other_sel .btn").click(function(){//Save opponent switch amount
+    	if($(this).attr("name") == "plus"){
+    		switch_other = switch_other + 1;
+    	}
+    	else{
+    		switch_other = switch_other - 1;
+    	}
+    	
+    	$("#switch_other").text(switch_other);
+    });
+    $("#scale_sel .btn").click(function(){//Save switch amount
+    	if($(this).attr("name") == "plus"){
+    		scale = scale + 1;
+    	}
+    	else{
+    		scale = scale - 1;
+    	}
+    	
+    	$("#scale").text(scale);
+    });
+    $("#exchange_sel .btn").click(function(){//Save exchange amount
+    	if($(this).attr("name") == "plus"){
+    		exchange = exchange + 1;
+    	}
+    	else{
+    		exchange = exchange - 1;
+    	}
+    	
+    	$("#exchange").text(exchange);
+    });
+    $("#auto_back_btn").click(function(){//Return to Auto scouting
+    	$("#auto").toggleClass("display");
+    	$("#teleop").toggleClass("display");
+    });
+    $("#endgame_btn").click(function(){//Go on to End Game scouting
+    	if(switch_personal >= 0 && switch_other >= 0 && scale >= 0 && exchange >= 0){
+    		$("#teleop").toggleClass("display");
+    		$("#endgame").toggleClass("display");
+    	}
+    	else{
+    		alert("Please fill in all information before moving on!");
+    	}
+    });
+    
+    //Endgame scouting scripts
+    $("#endgame_sel .btn").click(function(){//Save ending position
+    	endgame = $(this).attr("name");
+    });
+    $("#result_sel .btn").click(function(){//Save game result
+    	result = $(this).attr("name");
+    });
+    $("#comments").change(function(){//Save scouter's comments
+    	comments = $("#comments").val();
+    });
+    $("#teleop_back_btn").click(function(){//Return to Tele-Op scouting
+    	$("#teleop").toggleClass("display");
+    	$("#endgame").toggleClass("display");
+    });
+    $("#submit_btn").click(function(){//Submit data into AJAX request
+    	if(endgame !== "" && result !== "" && comments !== ""){
+    		submitData();
+    	}
+    	else{
+    		alert("Please fill in all information before moving on!");
+    	}
+    });
 });
-
