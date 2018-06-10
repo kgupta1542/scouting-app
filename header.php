0a7bcfc9b305f0391a5f69a6977b78d306f1aa00
Kanishk Gupta <35873683+kgupta1542@users.noreply.github.com>
2018-06-10 10:07:54 -0700

    Update header.php

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 			</div>
 			
 			<?php 
-				$dbc = mysqli_connect("localhost","root","C_BiscuitApp","my-db","3306");
+				$dbc = mysqli_connect("localhost","root","password","schema1","3306");
 		
 				@session_start();
 				@$id = $_SESSION["user_id"];
